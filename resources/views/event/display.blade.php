4b794c756997814f9d695b845771cc9d1685ed48
Bouke Regnerus <bouke@regner.us>
2016-06-04 21:29:26 +0200

    Layout Events Page

--- resources/views/event/display.blade.php
+++ resources/views/event/display.blade.php
@@ -6,27 +6,49 @@
 
 @section('content')
 
-    <div class="row">
+    <div class="row vevent">
 
         <div class="col-md-{{ ($event->activity && $event->activity->participants ? '8' : '8 col-md-offset-2') }}">
 
             <div class="panel panel-default">
 
-                <div class="panel-heading" style="text-align: center;">
+                <div class="panel-heading" style="text-align: center;">{{ $event->title }}</div>
 
-                    From {{ date('l j F, H:i', $event->start) }} till
+                <div class="panel-body summary" id="event-description">
+                    <div class="media">
+                        <div class="media-left media-middle">
+                            <i class="fa fa-3x fa-calendar-o" aria-hidden="true"></i>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">
+                                <abbr class="dtstart" title="{{ date('c', $event->start) }}">{{ date('l j F, H:i', $event->start) }}</abbr>
+                            </h4>
 
-                    @if (($event->end - $event->start) < 3600 * 24)
-                        {{ date('H:i', $event->end) }}
-                    @else
-                        {{ date('l j F, H:i', $event->end) }}
-                    @endif
+                            Untill
 
-                    @ {{ $event->location }}
+                            @if (($event->end - $event->start) < 3600 * 24)
+                                <abbr class="dtstart" title="{{ date('c', $event->end) }}">{{ date('H:i', $event->end) }}</abbr>
+                            @else
+                                <abbr class="dtstart" title="{{ date('c', $event->end) }}">{{ date('l j F, H:i', $event->end) }}</abbr>
+                            @endif
+                        </div>
+                    </div>
 
-                </div>
+                    <br>
 
-                <div class="panel-body" id="event-description">
+                    <div class="media">
+                        <div class="media-left media-middle">
+                            <i class="fa fa-3x fa-map-marker" aria-hidden="true"></i>
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">
+                                {{ $event->location }}
+                            </h4>
+                            <a href="https://www.google.nl/maps/search/{{ $event->location }}/">View on Google Maps</a>
+                        </div>
+                    </div>
+
+                    <hr>
 
                     {!! $event->description !!}
 
@@ -83,29 +105,42 @@
         @if($event->activity && $event->activity->participants)
 
             <div class="col-md-4">
-
                 <div class="panel panel-default">
-
                     <div class="panel-heading" style="text-align: center;">
                         Sign-up
                     </div>
 
                     <div class="panel-body" id="event-description">
+                        <p>
+                            <button type="button" class="btn btn-primary btn-lg btn-block">Join and RSVP &mdash; &euro;{{ $event->activity->price }}</button>
+                        </p>
+
+                        <hr>
+
                         @if (date('U') < $event->activity->registration_start)
-                            <p style="text-align: center;">
-                                Open between {{ date('M d, H:i', $event->activity->registration_start) }}
-                                and {{ date('M d, H:i', $event->activity->registration_end) }}.
+                            <p>
+                                Open between <abbr class="dtstart" title="{{ date('c', $event->activity->registration_end) }}">{{ date('l j F, H:i', $event->activity->registration_start) }}
+                                and <abbr class="dtstart" title="{{ date('c', $event->activity->registration_end) }}">{{ date('l j F, H:i', $event->activity->registration_end) }}</abbr>.
+
                             </p>
                         @elseif (date('U') < $event->activity->registration_end)
-                            <p style="text-align: center;">
-                                Sign-up closes {{ date('M d, H:i', $event->activity->registration_end) }}.
+                            <p>
+                                Sign-up closes <abbr class="dtstart" title="{{ date('c', $event->activity->registration_end) }}">{{ date('l j F, H:i', $event->activity->registration_end) }}</abbr>.
                             </p>
                         @else
-                            <p style="text-align: center;">
+                            <p>
                                 Sign-up closed.
                             </p>
                         @endif
-                        <hr>
+                    </div>
+                </div>
+
+                <div class="panel panel-default">
+                    <div class="panel-heading" style="text-align: center;">
+                        Participants
+                    </div>
+
+                    <div class="panel-body" id="event-description">
                         @foreach($event->activity->users as $user)
                             <div class="member">
                                 <div class="member-picture"
@@ -114,7 +149,6 @@
                             </div>
                         @endforeach
                     </div>
-
                 </div>
 
             </div>
